130e90ec290bb9d9700bf4646ef40de63ade45b1
Ostentive <joshuaolaniyi2017@gmail.com>
2023-02-18 13:39:15 +0000

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return Always 0
+ * 
+ * Here is my first program on Alx
+ */
+int main(void);
+{
+	printf("Programming is like building a multilingual puzzle"\n);
+	return (0);
+}
