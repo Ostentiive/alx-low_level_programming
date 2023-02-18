e100f019285973e2299c6089d283f8e8af790633
Ostentive <joshuaolaniyi2017@gmail.com>
2023-02-18 16:50:36 +0000

    C_Hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - Entry point 
+/*
  *
- * Return Always 0
- * 
- * Here is my first program on Alx
+ * Return Always 0 (success)
+ *
  */
-int main(void);
+int main(void)
 {
-	printf("Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
